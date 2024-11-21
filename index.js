be55befec8f4931891834bf8bc22ca0fcfbaee4a
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-21 14:26:01 +0100

    add convertToRoman() = version 1

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const inputNumber = document.getElementById("number");
-const convertBtn = document.getElementById("convert-btn");
+const numberInput = document.getElementById("number");
 const output = document.getElementById("output");
-// console.log(inputNumber);
-// console.log(convertBtn);
-// console.log(output);
+const convertBtn = document.getElementById("convert-btn");
 
-const checkUserInput = () => {
-  const inputValue = parseInt(inputNumber.value);
-  if (!inputValue) {
+const romanNumerals = [
+  { value: 1000, symbol: "M" },
+  { value: 900, symbol: "CM" },
+  { value: 500, symbol: "D" },
+  { value: 400, symbol: "CD" },
+  { value: 100, symbol: "C" },
+  { value: 90, symbol: "XC" },
+  { value: 50, symbol: "L" },
+  { value: 40, symbol: "XL" },
+  { value: 10, symbol: "X" },
+  { value: 9, symbol: "IX" },
+  { value: 5, symbol: "V" },
+  { value: 4, symbol: "IV" },
+  { value: 1, symbol: "I" },
+];
+
+const checkInput = () => {
+  const num = parseInt(numberInput.value, 10);
+
+  if (isNaN(num)) {
     output.textContent = "Please enter a valid number";
-  } else if (inputValue < 0) {
+  } else if (num < 1) {
     output.textContent = "Please enter a number greater than or equal to 1";
-  } else if (inputValue > 3999) {
-    output.textContent = "Please enter a number less than or equal to 3999.";
+  } else if (num >= 4000) {
+    output.textContent = "Please enter a number less than or equal to 3999";
   } else {
-    output.textContent = `Coverting:  ${inputValue}`;
-    return;
+    output.textContent = convertToRoman(num);
   }
-  inputNumber.value = "";
 };
 
-convertBtn.addEventListener("click", checkUserInput);
+const convertToRoman = (num) => {
+  let roman = "";
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      roman += romanNumerals[i].symbol;
+      num -= romanNumerals[i].value;
+    }
+  }
+  return (output.textContent = roman);
+};
+
+convertBtn.addEventListener("click", () => checkInput());
