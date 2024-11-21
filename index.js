ed0a4794d701419f670a403cf5e7ac5f92701fdb
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-21 09:18:03 +0100

    Declare const inputNumber, convertBtn, output

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Welcome!");
+const inputNumber = document.getElementById("number").value;
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+console.log(inputNumber);
+console.log(convertBtn);
+console.log(output);
